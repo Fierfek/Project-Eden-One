3d3e98e249f99f190296b8cb97d9d5fe6fdee3d2
LAPTOP-9DQS12E1\Henry Ding <henry1998628@gmail.com>
2018-09-21 22:25:19 -0700

    This is the demo of Tilemap Environment from Henry Ding (Prog-Environment Lead). It has the colliders implemented to the demo.

---
+++ Prog-Environment Demo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    Rigidbody2D rb;
+
+    public GameObject player;
+
+    private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+        rb = player.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        getDirection();
+        rb.MovePosition(rb.position + (direction * speed * Time.deltaTime));
+	}
+
+    void getDirection()
+    {
+        direction = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            direction = Vector2.up;
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction = Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = Vector2.right;
+        }
+    }
+}
