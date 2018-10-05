e50b5eb61dc7e6e891fc333e292bec197e8fe178
senirusan13 <36501296+senirusan13@users.noreply.github.com>
2018-10-05 14:37:52 -0700

    Quit Button Script preset

---
+++ The Curious Life of Eden/Assets/QuitButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		public void Quit()
+        {
+#if Unity_Editor
+            Unity.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
